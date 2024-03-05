e67f1cbdc19b9ebf739d10765836abda71ae5408
Sebastian <sebastian9705@gmail.com>
2024-03-05 21:17:45 +0100

    aggiunto layout

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="flex p-8">
+    <div>
+        <div class="w-[32px] h-[32px] rounded-full bg-indigo-600"></div>
+    </div>
+    <div class="flex-grow"></div>
+    <div class="text-white">
+        <button class="main-color-2 rounded-full p-2 border border-emerald-300">Registrati o accedi</button>
+        <button class="main-bg-color-2 rounded-full p-2"><i class="fa-solid fa-plus"></i>Vendi</button>
+    </div>
+</nav>
