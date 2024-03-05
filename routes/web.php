5e523c94d558a3e23e44733b0f680da4383b96f9
Emanuele Trupia <emanueletrupia1@gmail.com>
2024-03-05 21:57:33 +0100

    route e migration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomepageController;
+use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomepageController::class,'homepage'])->name('homepage');
+Route::get('/dashboard', Dashboard::class)->name('dashboard');
