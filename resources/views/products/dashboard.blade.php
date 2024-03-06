6b12e9e48d0f63a8cd1163667ce74d1bfe9a707b
Nicola De Angelis <deangelisnicola1996@hotmail.com>
2024-03-06 18:49:51 +0100

    crud

---
+++ resources/views/products/dashboard.blade.php
@@ -0,0 +1,9 @@
+
+{{-- navbar di Sebastian --}}
+
+<div class="text-2xl" >
+    Ciao NomeUtente
+</div>
+<br>
+<button class="text-lg text-green-400" > <a href="product.create"> Inserisci il tuo prodotto </a>
+    </button>
---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,19 @@
+<x-main>
+		<div class="grid-cols-2">
+			<div class="row gx-4 gx-lg-5 align-items-center">
+				<div>
+					 
+					<img style="width:20rem" src="{{Storage::url($product->image)}}" alt="..." />
+				</div>
+				<div>
+				 
+					<h1>{{$product->object}}</h1>
+					<p>Categoria: {{$product->category}}</p>
+					<p>Prezzo: {{$product->price}}</p> 
+					
+                     <p>Descrizione: {{$product->about}}</p>
+					<hr> 
+				</div>
+			</div>
+	</section>
+</x-main>
