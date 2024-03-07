b9f3e323800f2217485403b32c874c7c6d25c7f6
Nicola De Angelis <deangelisnicola1996@hotmail.com>
2024-03-07 20:53:00 +0100

    termine category2

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,14 +1,16 @@
 <article class="md:w-1/6 rounded-lg text-slate-400">
+    @forelse ($category->products as $product )
     <a href="/">
         <img class="rounded-lg" src="{{asset('images/product-placeholder.jpg')}}" alt="">
         <div class="flex justify-between mt-2">
-            <span class="font-bold">100 €</span>
-            <p>category</p>
+            <span class="font-bold">{{$price}}</span>
+            <p>{{$category}}</p>
         </div>
-        <h3 class="mt-2">Titolo</h3>
+        <h3 class="mt-2">{{$title}}</h3>
         <div class="flex text-purple-500 mt-2">
          <i class="fa-solid fa-truck-fast"></i>
          <span class="text-sm ms-2">Spedizione disponibile</span>
+         @endforelse
         </div>
     </a>
 </article>
--- resources/views/products/categoryShow.blade.php
+++ resources/views/products/categoryShow.blade.php
@@ -11,21 +11,9 @@ public function categoryShow(Category $category)
 {
     return view('categoryShow', compact('category'));
 }  --}}
-<article class="md:w-1/6 rounded-lg text-slate-400">
-    @forelse ($category->products as $product )
-    <a href="/">
-        <img class="rounded-lg" src="{{asset('images/product-placeholder.jpg')}}" alt="">
-        <div class="flex justify-between mt-2">
-            <span class="font-bold">{{$product->price}}</span>
-            <p>{{$product->category}}</p>
-        </div>
-        <h3 class="mt-2">{{$product->title}}</h3>
-        <div class="flex text-purple-500 mt-2">
-         <i class="fa-solid fa-truck-fast"></i>
-         <span class="text-sm ms-2">Spedizione disponibile</span>
-         @empty
-         <h2 class="mt-2">Prodotto non trovato</h3>
-         @endforelse
-        </div>
-    </a>
-</article>
+
+
+
+@forelse ($category->products as $product )
+<x-product-card price="{{$product->price}}" category={{$product->category}} title={{$product->title}}/>
+@endforelse 
