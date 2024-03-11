0f10aaeae69a4efd0d8f5ddb951fc1a562e252f8
Nicola De Angelis <deangelisnicola1996@hotmail.com>
2024-03-11 20:35:49 +0100

    modifiche

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -19,7 +19,7 @@
                     class="fa-solid fa-plus me-2 border border-white rounded-full p-1"></i>Vendi</a></button>
                     </div>
                     <div class="relative inline-block text-left">
-                        <div>
+                         <div>
                           <button type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
                             Categoria
                             <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -36,5 +36,5 @@
                           </div>
                         </div>
                       </div>
-                      
-</nav>
+                       
+ </nav>
