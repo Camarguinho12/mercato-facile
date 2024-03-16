d208b320df5852e23859882dac324f2c61f5f617
Sebastian <sebastian9705@gmail.com>
2024-03-16 10:50:21 +0100

    aggiunto animazione grafico statistiche

--- resources/views/livewire/dashboard/stats.blade.php
+++ resources/views/livewire/dashboard/stats.blade.php
@@ -2,46 +2,53 @@
     <h1 class="text-center text-4xl zoom-in my-8 fade-top">Le tue statistiche</h1>
     <div id="chart_div"></div>
     <script>
-        google.charts.load("current", { packages: ["corechart"] });
-    
+        google.charts.load("current", {
+            packages: ["corechart"]
+        });
+
         google.charts.setOnLoadCallback(drawChart);
-    
+
         function drawChart() {
             var data = new google.visualization.DataTable();
             data.addColumn("string", "Topping");
             data.addColumn("number", "numero annunci");
             data.addRows(@json($data));
-    
+
             var options = {
+                animation: {
+                    startup:true,
+                    duration: 1000,
+                    easing: 'out',
+                },
                 title: "I tuoi annunci pubblicati",
                 titleTextStyle: {
                     fontSize: 18,
-                    fontName: "Madimi One", 
-                    color: "#333" 
+                    fontName: "Montserrat",
+                    color: "#333"
                 },
                 legend: {
                     textStyle: {
-                        fontSize: 14, 
-                        fontName: "Madimi One", 
-                        color: "#666" 
+                        fontSize: 14,
+                        fontName: "Montserrat",
+                        color: "#666"
                     }
                 },
                 colors: ['#13c1ac'],
                 hAxis: {
-                textStyle: {
-                    fontSize: 12, // Dimensione del font delle etichette
-                    fontName: "Madimi One", // Nome del font delle etichette
-                    color: "#666" // Colore delle etichette
-                }
-            },
-            backgroundColor: 'rgb(249,250,251)' 
+                    textStyle: {
+                        fontSize: 12,
+                        fontName: "Montserrat",
+                        color: "#666"
+                    },
+                },
+                backgroundColor: 'rgb(249,250,251)'
             };
-    
+
             var chart = new google.visualization.ColumnChart(
                 document.getElementById("chart_div")
             );
             chart.draw(data, options);
         }
     </script>
-    
+
 </div>
