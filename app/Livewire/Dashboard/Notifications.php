f80caa374ec8bf02600c82e8c6fa51e00ab80c31
Sebastian <sebastian9705@gmail.com>
2024-03-20 21:25:33 +0100

    sistemato accorgimenti notifiche

--- app/Livewire/Dashboard/Notifications.php
+++ app/Livewire/Dashboard/Notifications.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Dashboard;
 
+use App\Models\User;
+use App\Notifications\RevisorFeedback;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
@@ -10,11 +12,40 @@ use Livewire\Component;
 class Notifications extends Component
 {
     public $userNotifications;
+    public $message;
+    public $showMessage=false;
 
     public function mount(){
         foreach(Auth::user()->notifications as $notification){
             $this->userNotifications[]=$notification->data;
         };
+    }
+
+    public function acceptRevisor($key){
+        $userToBeAccepted=User::find($this->userNotifications[$key]['id']);
+        if($userToBeAccepted->is_revisor == 1){
+            $this->setMessage('utente già revisore');
+            return;
+        }
+        else{
+            $userToBeAccepted->is_revisor=true;
+            $userToBeAccepted->save();
+            $this->setMessage('utente accettato come revisore');
+            $userToBeAccepted->notify(new RevisorFeedback('sei stato accettato come revisore'));
+        }
+    }
+
+    public function denyRevisor($key){
+        $userToBeDenied=User::find($this->userNotifications[$key]['id']);
+        if($userToBeDenied->is_revisor == 0){
+            $this->setMessage('utente già senza privilegi da amministratore');
+            return;
+        }
+    }
+
+    public function setMessage(String $message):void{
+        $this->message=$message;
+        $this->showMessage=!$this->showMessage;
     }
 
 
